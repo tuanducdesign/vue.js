d4e860d42c482031b20d9200b43bd739f44a02be
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-03-04 23:41:34 +0700

    Update test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueMeta from 'vue-meta'
-Vue.use(VueMeta)
-
+import './assets/styles/app.scss'
 Vue.config.productionTip = false
 
 new Vue({
