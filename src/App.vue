ebfd695cbc010a661aac984026bc1ba48481f424
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-03-04 22:03:30 +0700

    Test

--- src/App.vue
+++ src/App.vue
@@ -30,23 +30,23 @@ export default {
 mounted() {
       // Jquery
       let jqueryScript = document.createElement('script')
-      jqueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js')
+      jqueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js','defer')
       document.head.appendChild(jqueryScript)
       // Bootstrap bundle
       let bundleScript = document.createElement('script')
-      bundleScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js')
+      bundleScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js','async','1')
       document.head.appendChild(bundleScript)
       // Popper
       let popperScript = document.createElement('script')
-      popperScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js')
+      popperScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js','async')
       document.head.appendChild(popperScript)
       // Bootstrap
       let bootstrapScript = document.createElement('script')
-      bootstrapScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js')
+      bootstrapScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js','async')
       document.head.appendChild(bootstrapScript)
       // Custom
       let mainScript = document.createElement('script')
-      mainScript.setAttribute('src','https://cdn.tuanducdesign.com/assets/js/main.min.js')
+      mainScript.setAttribute('src','https://cdn.tuanducdesign.com/assets/js/main.min.js','async')
       document.head.appendChild(mainScript)
     },
 }
