447da4701c7c559740e95728097600e6bc2c587a
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2021-03-04 23:07:08 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 // Navication
 import Navication from './components/Navication.vue'
 // Header
-//import Banner from './components/Banner.vue'
+import Banner from './components/Banner.vue'
 // About
 //import About from './components/About.vue'
 // Exp
@@ -26,29 +26,7 @@ export default {
   name: 'app',
   components: {
     Navication
-  },
-mounted() {
-      // Jquery
-      let jqueryScript = document.createElement('script')
-      jqueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js','defer')
-      document.head.appendChild(jqueryScript)
-      // Bootstrap bundle
-      let bundleScript = document.createElement('script')
-      bundleScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js','async','1')
-      document.head.appendChild(bundleScript)
-      // Popper
-      let popperScript = document.createElement('script')
-      popperScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js','async')
-      document.head.appendChild(popperScript)
-      // Bootstrap
-      let bootstrapScript = document.createElement('script')
-      bootstrapScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js','async')
-      document.head.appendChild(bootstrapScript)
-      // Custom
-      let mainScript = document.createElement('script')
-      mainScript.setAttribute('src','https://cdn.tuanducdesign.com/assets/js/main.min.js','async')
-      document.head.appendChild(mainScript)
-    },
+  }
 }
 </script>
 
---
+++ src/components/Banner.vue
@@ -0,0 +1,26 @@
+<template>
+ <header class="header-container" itemtype="https://schema.org/WPHeader" itemscope="itemscope" role="banner" id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h1 itemprop="name" class="info-landing">Hi! Mình là <span>Trần Tuấn Đức</span></h1>
+                    <div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+                        <img class="profile mmo-float img-thumbnail" src="https://cdn.tuanducdesign.com/assets/images/tuanducdesign.jpg" alt="Trần Tuấn Đức" loading="lazy" />
+                        <meta itemprop="url" content="https://cdn.tuanducdesign.com/assets/images/tuanducdesign.jpg" />
+                        <meta itemprop="width" content="128" />
+                        <meta itemprop="height" content="128" />
+                    </div>
+                    <p itemprop="description" class="info-desc">Chào mừng bạn đến với trang profile của mình</p>
+                    <a href="#vetoi" class="vetoi" address="true" title="Về tôi"></a>
+                </div>
+            </div>
+        </div>
+</header>
+</template>
+<script>
+export default {
+  name: 'Banner',
+  props: {
+  }
+}
+</script>
